3c911052c04c8a83610dfe42012b21bc17a6a56d
Maher Ali <maher.ali@gmail.com>
2011-06-26 17:49:35 -0500

    first commit

---
+++ src/com/agilismobility/com/UGotFlaggedActivity.java
@@ -0,0 +1,13 @@
+package com.agilismobility.com;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UGotFlaggedActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
