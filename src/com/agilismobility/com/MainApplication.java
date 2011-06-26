6f14d661591ddd3a23018875d3827044c14833ad
Maher Ali <maher.ali@gmail.com>
2011-06-26 18:00:00 -0500

    added app class

---
+++ src/com/agilismobility/com/MainApplication.java
@@ -0,0 +1,26 @@
+package com.agilismobility.com;
+
+import android.app.Application;
+
+public class MainApplication extends Application{
+
+	@Override
+	public void onCreate() {
+		// TODO Auto-generated method stub
+		super.onCreate();
+	}
+	
+	@Override
+	public void onLowMemory() {
+		// TODO Auto-generated method stub
+		super.onLowMemory();
+	}
+	
+	@Override
+	public void onTerminate() {
+		// TODO Auto-generated method stub
+		super.onTerminate();
+	}
+	
+	
+}
