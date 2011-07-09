adcffc29bb6418e3673837609bf14bb501ff1ad7
Maher Ali <maher.ali@gmail.com>
2011-07-08 20:52:30 -0500

    started teh register layout

--- src/com/agilismobility/ugotflagged/UI/LoginActivity.java
+++ src/com/agilismobility/ugotflagged/UI/LoginActivity.java
@@ -7,6 +7,9 @@ import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -135,4 +138,23 @@ public class LoginActivity extends BaseActivity {
 		startActivity(newIntent);
 	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.register_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.register:
+			Intent intent = new Intent();
+			intent.setClass(getApplication(), RegisterActivity.class);
+			startActivity(intent);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
 }
---
+++ src/com/agilismobility/ugotflagged/UI/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.agilismobility.ugotflagged.UI;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.agilismobility.ugotflagged.R;
+
+public class RegisterActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.register);
+	}
+
+}
